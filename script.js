08bcb0a5a9d8b235fdb7e81c4f7351e9b9355843
Vida Padilla <vidap04301996@gmail.com>
2023-07-25 08:15:59 +0800

    Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".text", {
+    strings: ["FrontEnd Developer" , "BackEnd Developer" , "Full-Stack Web Developer" , "Web Developer" , "Student of KodeGo 2023"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
